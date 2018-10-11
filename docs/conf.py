b9632e9bce1b43568241a3d829f0fee009a88d4d
wieschie <supernova190@gmail.com>
2018-10-11 15:17:45 -0500

    Bump version: 0.0.1 → 0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Evan Schiewe'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
