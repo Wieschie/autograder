dcfcbe09b1d584a119b9a5192a3e7f7a6772af55
wieschie <supernova190@gmail.com>
2018-10-23 13:33:19 -0500

    Remove unnecesary placeholder code.

--- autograder/Config.py
+++ autograder/Config.py
@@ -11,10 +11,9 @@ from pathlib import Path
 
 class Config:
 
-    def __init__(self):
-        self.__config_str = ""
+    def __init__(self, filename):
         self.__config_dict = dict()
-        self.__load_config()
+        self.__load_config(filename)
 
     def __contains__(self, item):
         return item in self.__config_dict
@@ -22,24 +21,22 @@ class Config:
     def __getitem__(self, item):
         return self.__config_dict[item]
 
-    def replace(self, student_dir: Path):
-        """ replace placeholder values """
-        tmp = self.__config_str.replace("<student>", str(student_dir))
-        self.__config_dict = toml.loads(tmp)
-
-    def __load_config(self):
+    def __load_config(self, filename):
         """ loads config.toml from working directory and attempts to validate it with some simple rules"""
         try:
-            with open("config.toml", "r") as f:
-                self.__config_str = f.read()
-                self.__config_dict = toml.loads(self.__config_str)
-                self.__validate_config()
+            self.__config_dict = toml.load(filename)
         except FileNotFoundError:
             raise FileNotFoundError("No config file found.  Are you in the root directory of a project?")
 
+        self.__validate_config()
+
     def __validate_config(self):
-            schema = anyconfig.load("config_schema.json")
-            try:
-                anyconfig.validate(self.__config_dict, schema, ac_schema_safe=False)
-            except ValidationError as e:
-                raise ValidationError(f"Invalid config file:\n{e.message}")
+        schema = anyconfig.load("config_schema.json")
+        try:
+            anyconfig.validate(self.__config_dict, schema, ac_schema_safe=False)
+        except ValidationError as e:
+            raise ValidationError(f"Invalid config file:\n{e.message}")
+
+    def get(self, key):
+            return self.__config_dict.get(key)
+
