c0def79615ad23341d90dba4c08145083abf5d07
Evan Schiewe <supernova190@gmail.com>
2018-11-13 15:56:47 -0600

    Bump version: 0.4.0 → 0.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Evan Schiewe"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.4.0"
+release = "0.5.0"
 
 
 # -- General configuration ---------------------------------------------------
