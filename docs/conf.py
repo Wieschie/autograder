57b175887c9daeb4097ef858d935139d5077fe0f
Evan Schiewe <supernova190@gmail.com>
2018-11-27 15:40:07 -0600

    Convert dev log to reST, remove recommonmark from sphinx.

--- docs/conf.py
+++ docs/conf.py
@@ -54,10 +54,10 @@ templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = [".rst", ".md"]
+source_suffix = [".rst"]
 
 # Add support for Markdown files.
-source_parsers = {".md": "recommonmark.parser.CommonMarkParser"}
+# source_parsers = {".md": "recommonmark.parser.CommonMarkParser"}
 
 # The master toctree document.
 master_doc = "index"
